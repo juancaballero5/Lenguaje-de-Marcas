6c251f8d2d29a5022651e34c5ecea5df11f7f5ab
Juan Caballero Castillo <jcabcas2806@g.educaand.es>
2026-01-23 14:43:27 +0100

    caca

--- Unidad 03/dom-intro/app.js
+++ Unidad 03/dom-intro/app.js
@@ -1,1 +1,26 @@
 "use strict";
+
+/**
+ * Metoddos de busqueda
+ * 
+ * - .getElementsById(id) -> Busca elementos con su id.
+ * - .getElementsByClassName(class) -> Busca por nombre de clase.
+ * - .getElementsByName(value) -> Busca elemento con el atributo name y el valor que le corresponda.
+ * - .getElementsByTagName(tag) -> Busca etiquetas HTML tag.
+ * 
+ * */
+
+const app = document.querySelector("#app");
+
+const name = "1º DAW";
+
+app.textContent = `Hola ${name}`;
+app.textContent = "<p>Hola Sergio</p>";
+
+app.innerHTML = "<strong>Hola Sergio</strong>";
+app.innerHTML = `
+<section>
+    <p>Espero que esteis aprendiendo algo
+    <strong>Menos mal que empieza el finde.</strong>
+    </p>
+</section>`;
