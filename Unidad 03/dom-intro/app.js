203c80a9c49f66b436d81bbdb4338b542b518948
Juan Caballero Castillo <jcabcas2806@g.educaand.es>
2026-01-30 13:55:54 +0100

    caca

--- Unidad 03/dom-intro/app.js
+++ Unidad 03/dom-intro/app.js
@@ -26,5 +26,33 @@ app.innerHTML = `
 </section>`;
 
 // Métodos modernos para modificar contenido
+// console.log("Método moderno GET HTML: " + app.getHTML());
+const cod = `<p><strong>setHTMLUnsafe</strong> es la alternativa moderna 
+a innerHTML para modificar contenido HTML</p>`;
+app.setHTMLUnsafe(cod);
 
-console.log(app.getHTML());
+// Crear elementos HTML
+// Método createElement("etiqueta")
+const seccion = document.createElement("section");
+console.log("El elemento seccion aun no está conectado o insertado:", seccion.isConnected);
+seccion.innerHTML = `<p>Estoy inyectando código HTML a la sección</p>`;
+
+// Añado la nueva sección al div cuyo id es app
+app.appendChild(seccion);
+console.log(seccion.isConnected);       //true porque ya está insertada
+
+// Si quisiera clonar el elemento seccion
+const copiaSeccion = seccion.cloneNode();
+const copiaProfunda = seccion.cloneNode(true);
+
+// Mini reto:
+const div = document.querySelector(".container");
+const parrafo = document.createElement("p");
+parrafo.innerHTML = `Este es el primer párrafo`;
+const articulo = document.createElement("article");
+const negrita = document.createElement("strong");
+negrita.textContent = `Este es el texto que aparecerá en negrita y en verde`;
+div.appendChild(parrafo);
+div.appendChild(articulo);
+articulo.appendChild(negrita);
+negrita.className = "green";
