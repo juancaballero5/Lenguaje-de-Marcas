2c6a5cc5481d75717a68a5b42fc2597b0c76d087
juancaballero5 <jcabcas2806@g.educaand.es>
2026-03-18 16:28:07 +0100

    caca

---
+++ Unidad 04/LMSGI_JuanCaballeroCastillo_Proyecto/js/javascript.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const ENDPOINT = "https://rickandmortyapi.com/api/";
+
+    const boton = document.getElementById("boton");
+    const imagenPersonaje = document.getElementById("juego");
+
+});
